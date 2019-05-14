19f2c3aa65fc66de560b18f594ee025126f0dae6
Nikita Soifer <soifernikita01@gmail.com>
2019-05-14 09:10:07 -0700

    Add files via upload

---
+++ assets/js/analytics.js
@@ -0,0 +1,4 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-140119780-1"></script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
